dd60c37ddf9ae9476bcd02726627fc7ce6c445b5
Rafael Pereira <rafaelplaurindo@gmail.com>
2021-05-03 19:27:52 -0300

    adds AsyncPromise class

---
+++ src/async-promise.ts
@@ -0,0 +1,128 @@
+// acho que saporra pode ser substituída por uma async functions com await 🤦. "Vão" ver.
+
+export class AsyncPromise<T> {
+
+    private status!: string;
+
+    private promise!: Promise<T>;
+
+    private currentTimeout!: number;
+
+    private _accomplishResponse: any;
+
+    private _then!: (response: any) => void;
+
+    private _reject!: () => void;
+
+    private whenReject!: () => void;
+
+    private self: AsyncPromise<T>;
+
+    constructor (options: object, resolver: () => void) {
+        this.self = this;
+
+        const resolveArguments: () => void = () => {
+            if (typeof options == "function") {
+                resolver = options as () => void;
+                options = {};
+            }
+        }
+
+        resolveArguments();
+
+        this.start(resolver, options);
+    }
+
+    break (run: () => void) {
+        if (typeof run != "function") {
+            run = () => { };
+        }
+
+        this.whenReject = run;
+
+        clearTimeout(this.currentTimeout);
+        this._reject();
+    }
+
+    origin(): Promise<T> {
+        // to work with async functions
+        return this.promise;
+    };
+
+    then(callback: () => void) {
+        this._then = callback;
+    };
+
+    catchException(run: () => void): AsyncPromise<T> {
+        if (typeof run != "function") {
+            run = function () { };
+        }
+
+        this.promise.catch(run);
+
+        return this;
+    };
+
+    private accomplish(response: any) {
+        // "fulfilled"
+        this.status = "resolved";
+        this._accomplishResponse = response;
+    }
+
+    private start(resolver: () => void, options: any) {
+
+        if (typeof (options.defer != "number")) {
+            options.defer = 0;
+        }
+
+        const loop: (_accomplish: (response?: any) => void, _reject: () => void, resolver: (response?: any) => void
+            , options: any) => void = (
+                _accomplish: (response?: any) => void
+                , _reject: () => void
+                , resolver: (response?: any) => void
+                , options: any
+            ) => {
+
+                this.currentTimeout = window.setTimeout(
+                    () => {
+                        if (typeof resolver != "function" ||
+                            (options.defer &&
+                                typeof resolver != "function")) {
+                            _accomplish();
+                        } else {
+                            options.defer = 0;
+                            resolver.call(this.self, this.accomplish);
+                            // returns the control for the user
+                            switch (status) {
+                                case "resolved": {
+                                    _accomplish(this._accomplishResponse);
+                                }
+                                    break;
+                                default: {
+                                    loop(_accomplish, _reject, resolver, options);
+                                }
+                            }
+                        }
+                    },
+                    options.defer
+                );
+            };
+
+        this.promise = new Promise((_accomplish: (response: any) => void, reject: () => void) => {
+            this._reject = reject;
+            loop(_accomplish, reject, resolver, options);
+        });
+
+        this.promise.then(
+            // response comes from original Promise object
+            (r: any) => {
+                this._then(r);
+            },
+
+            () => {
+                this.whenReject();
+            }
+        );
+    };
+
+}
--- src/test.ts
+++ src/test.ts
@@ -1,15 +1,39 @@
-import { MathematicalStrategy } from './lib/roman-numeral-set/strategies';
-import { HumanStrategy } from './lib/roman-numeral-set/strategies/humanStrategy';
-import { StrategyContext } from './lib/roman-numeral-set/strategyContext';
+// import { MathematicalStrategy } from './lib/roman-numeral-set/strategies';
+// import { HumanStrategy } from './lib/roman-numeral-set/strategies/humanStrategy';
+// import { StrategyContext } from './lib/roman-numeral-set/strategyContext';
 
 
-const indoArabicValue: number = 12209;
-console.log(`Indo Arabic value: `, indoArabicValue);
+// const indoArabicValue: number = 12209;
+// console.log(`Indo Arabic value: `, indoArabicValue);
 
-let romanNumeral: StrategyContext;
+// let romanNumeral: StrategyContext;
 
-romanNumeral = new StrategyContext(new HumanStrategy(indoArabicValue));
+// romanNumeral = new StrategyContext(new HumanStrategy(indoArabicValue));
 
-// romanNumeral = new StrategyContext(new MathematicalStrategy(indoArabicValue));
+// // romanNumeral = new StrategyContext(new MathematicalStrategy(indoArabicValue));
 
-console.log(`Value converted to roman: `, romanNumeral.numeral);
+// console.log(`Value converted to roman: `, romanNumeral.numeral);
+
+let cachedInput: string = '';
+
+const readline = require("readline");
+const readLine = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const question = readLine.question("", function (input: string) {
+    cachedInput = input;
+});
+
+// const promise = new Promise(
+//     (input: ) => {
+
+//     }
+// );
+
+// while (cachedInput != 'exit') {
+//     question;
+// }
+
+console.log(question);
